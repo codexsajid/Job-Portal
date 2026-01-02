198b1e093b246915f34414b3b0d5e2631fb2beef
Sajid Khan <sajid05541786@gmail.com>
2026-01-02 14:32:39 +0530

    frontend url set

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: "https://job-portal-1-fapb.onrender.com",
     credentials: true
 }));
 
